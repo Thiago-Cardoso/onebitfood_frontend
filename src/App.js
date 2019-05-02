28a86c2e04ccc132858031a41d3362604bc3747e
Thiago-Cardoso <thiagocardosooti@gmail.com>
2019-05-01 21:44:45 -0300

    Created component header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import "./App.scss";
 import { Container, Section } from 'rbx';
+import Header from "./components/header";
  
 const App = () => (
-  <div>
+  <Fragment>
+    <Header />
     <Section>
       <Container>
         One Bit Food
       </Container>
     </Section>
-  </div>
+  </Fragment>
 )
  
 export default App;
