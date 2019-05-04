0e7b0b5a7c9d360cef0889a699d873c3f978c854
Thiago-Cardoso <thiagocardosooti@gmail.com>
2019-05-04 13:58:38 -0300

    load of categories

--- src/actions/restaurant.js
+++ src/actions/restaurant.js
@@ -1,8 +1,8 @@
 import { LOAD_RESTAURANTS } from "./action_types";
 import api from "../services/api";
  
-export const loadRestaurants = () => async (dispatch) => {
-  let response = await api.loadRestaurants();
+export const loadRestaurants = (category = null) => async (dispatch) => {
+  let response = await api.loadRestaurants(category);
   dispatch({
     type: LOAD_RESTAURANTS,
     restaurants: response.data.restaurants
--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,9 @@ import axios from "axios";
 const url = axios.create({ baseURL: "http://localhost:3001" })
  
 export default {
-  loadRestaurants: () => url.get("/restaurants")
+  loadRestaurants: (category = null) => {
+    let filter = category ? `?category=${category.title}` : ""
+    return url.get(`/restaurants${filter}`)
+  },
+  loadCategories: () => url.get("/categories")
 }
