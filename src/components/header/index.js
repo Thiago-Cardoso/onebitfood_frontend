1862b7a4756b1a7bc8c0a20a1f794954c85c7f1e
Thiago-Cardoso <thiagocardosooti@gmail.com>
2019-05-11 10:31:31 -0300

    component address_form

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
  
-import { Navbar, Container } from 'rbx';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+ 
+import { Navbar, Container, Icon } from 'rbx';
 import LogoImage from '../../assets/images/logo-v1-horizontal.png';
 import SearchBox from '../search_box_component';
+import { showModal } from "../../actions/modal";
+import { FaCrosshairs } from 'react-icons/fa';
+ 
  
 import "../../styles/header.scss";
  
-const Header = () => (
+const Header = (props) => (
   <div class="top-navbar">
     <Container>
       <Navbar>
@@ -18,10 +24,20 @@ const Header = () => (
             <Navbar.Segment as="div" class="navbar-item navbar-center">
               <SearchBox />
             </Navbar.Segment>
+            <Navbar.Segment as="div" align="end">
+              <Navbar.Item onClick={() => props.showModal('ADDRESS_MODAL')}>
+                <Icon color="has-custom-black" >
+                  <FaCrosshairs />
+                </Icon>
+                <p>Endereço</p>
+              </Navbar.Item>
+            </Navbar.Segment>
           </Navbar.Menu>
       </Navbar>
     </Container>
   </div>
 );
  
-export default Header;
+const mapDispatchToProps = dispatch => bindActionCreators({ showModal }, dispatch);
+ 
+export default connect(null, mapDispatchToProps)(Header);
